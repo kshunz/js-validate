63b40fd81ed125ecb7f0765707bccd2062c2a983
Dwayne Ford <dwayneford@gmail.com>
2015-05-15 00:53:27 -0400

    Package tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
-var gulp = require('gulp');
+var browserify = require('gulp-browserify');
 var chai = require('chai');
+var gulp = require('gulp');
+var minify = require('gulp-uglify');
 var mocha = require('gulp-mocha');
+var rename = require('gulp-rename');
 
+gulp.task('package', function() {
+  return gulp.src('./src/js-validate.js')
+    .pipe(browserify())
+    .pipe(gulp.dest('./'));
+});
+gulp.task('package-min', function() {
+  return gulp.src('./js-validate.js')
+    .pipe(minify({}))
+    .pipe(rename('js-validate.min.js'))
+    .pipe(gulp.dest('./'));
+});
 gulp.task('test', function() {
   global.expect = chai.expect;
-  
-  return gulp.src('tests/**/*.js')
+
+  return gulp.src('./tests/**/*.js')
     .pipe(mocha());
 });
 
-gulp.task('default', []);
+gulp.task('default', ['test', 'package']);

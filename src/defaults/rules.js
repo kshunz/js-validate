0deed92ecfa84fee8396cd070b5faa9799a5652d
Dwayne Ford <dwayneford@gmail.com>
2015-11-02 17:20:21 -0600

    Alpha plus, alphanum plus, number plus. Added functionality to data-type rules.

--- src/defaults/rules.js
+++ src/defaults/rules.js
@@ -1,38 +1,59 @@
-module.exports = {
-    'alpha': function(input) {
-        return /^[A-Za-z]{1,}$/.test(input);
-    },
-    'alphanumeric': function(input) {
-        return /^[A-Za-z0-9]{1,}$/.test(input);
-    },
-    'capitals': function(input, count) {
-        count = Number(count);
-        count = Number.isNaN(count) ? 1 : count;
+module.exports = function () {
+    var _ = require('lodash');
 
-        return new RegExp('^(.*[A-Z]){'+count+',}.*$').test(input);
-    },
-    'length': function(input, length) {
-        return String(input).length === Number(length);
-    },
-    'max-length': function(input, length) {
-        return String(input).length <= length
-    },
-    'min-length': function(input, length) {
-        return String(input).length >= length;
-    },
-    'number': function(input) {
-        return String(input) === String(Number(input));
-    },
-    'numbers': function(input, count) {
-        count = Number(count);
-        count = Number.isNaN(count) ? 1 : count;
+    return {
+        'alpha': function (input) {
+            var extraChars = Array.prototype.slice.call(arguments, 1);
+            var pattern = '^[A-Za-z' + _.flatten(extraChars) + ']+$';
 
-        return new RegExp('^(.*[0-9]){'+count+',}.*$').test(input);
-    },
-    'specials': function(input, count) {
-        count = Number(count);
-        count = Number.isNaN(count) ? 1 : count;
+            return new RegExp(pattern).test(input);
 
-        return new RegExp('^(.*[^A-Za-z0-9]){'+count+',}.*$').test(input);
-    }
-};
+        },
+        'alphanumeric': function (input) {
+            var extraChars = Array.prototype.slice.call(arguments, 1);
+            var pattern = '^[A-Za-z0-9' + _.flatten(extraChars) + ']+$';
+
+            return new RegExp(pattern).test(input);
+        },
+        'capitals': function (input, count) {
+            count = Number(count);
+            count = Number.isNaN(count) ? 1 : count;
+
+            return new RegExp('^(.*[A-Z]){' + count + ',}.*$').test(input);
+        },
+        'length': function (input, length) {
+            return String(input).length === Number(length);
+        },
+        'max-length': function (input, length) {
+            return String(input).length <= length
+        },
+        'min-length': function (input, length) {
+            return String(input).length >= length;
+        },
+        'number': function (input) {
+            var extraChars = Array.prototype.slice.call(arguments, 1);
+
+            extraChars.forEach(function(char) {
+                if(String(char).toLowerCase() === 'space') {
+                    char = ' ';
+                }
+
+                input = input.split(char).join('');
+            })
+
+            return String(input) === String(Number(input));
+        },
+        'numbers': function (input, count) {
+            count = Number(count);
+            count = Number.isNaN(count) ? 1 : count;
+
+            return new RegExp('^(.*[0-9]){' + count + ',}.*$').test(input);
+        },
+        'specials': function (input, count) {
+            count = Number(count);
+            count = Number.isNaN(count) ? 1 : count;
+
+            return new RegExp('^(.*[^A-Za-z0-9]){' + count + ',}.*$').test(input);
+        }
+    };
+}();

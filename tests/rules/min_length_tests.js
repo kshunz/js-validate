7ff241c5e509fb2c1244b2c8920e8cdc8f45ac68
Dwayne Ford <dwayneford@gmail.com>
2015-11-07 09:12:37 -0600

    min-length tests

---
+++ tests/rules/min_length_tests.js
@@ -0,0 +1,16 @@
+describe('Min-length validator', function() {
+  var minLength = RULES['min-length'];
+
+  it('should accept inputs equal to or less than a specified length', function() {
+    expect(minLength('hello', '5')).to.be.true;
+    expect(minLength('hello', '15')).to.be.false;
+    expect(minLength('hello', '1')).to.be.true;
+  });
+  it('should reject inputs less than a specified length', function() {
+    expect(minLength('hello', '15')).to.be.false;
+  });
+  it('should accept/reject empty strings based on min-length rule', function() {
+    expect(minLength('', '0')).to.be.true;
+    expect(minLength('', '2')).to.be.false;
+  });
+});

cb193cc842532aa51bf745f10b68bb5fd91e6fe0
Dwayne Ford <dwayne@bl.io>
2016-07-10 22:10:46 -0400

    new format for async version 1 validator. Class returns built validate function.

--- src/validate1.js
+++ src/validate1.js
@@ -1,43 +1,34 @@
-const RuleSets = require('../lib/rule_sets');
+const Rules = require('../lib/rule_sets');
 
 module.exports = class  {
-    
-    constructor () {
-        console.log('validator constructor...');
-        this.rulesets = function () {
-            return new RuleSets;
-        }
-    }
-    
-    __start () {
-        return new Promise(function (resolve) {
-            resolve();
-        });
-    }
 
     validate () {
-
         var rules = Array.prototype.slice.call(arguments);
         var input = rules.shift();
-        var rulesets = this.rulesets();
+        var rulesets = new Rules;
 
+        rules = rules[0];
+        rules = typeof rules === 'string' ? [rules] : rules;
+        
         var validators = rules.map(function (rule) {
             var params = typeof rule !== 'string' ? rule : rule.split(' ');
             var ruleName = params.shift();
+            var validatorContext = Object.create(null);
             var validator = new Promise(function (resolve) {
-                params.push(resolve);
+                validatorContext.resolve = resolve;
             });
 
             params.unshift(input);
 
-            rulesets[ruleName].apply(new WeakMap(), params);
+            rulesets[ruleName].apply(validatorContext, params);
 
             return validator;
-
         });
 
-        //@TODO: Making a mistake with my promise setups somehow, urghhhh
-        return Promise.all(validators)
-
+        return Promise.all(validators);
+    }
+    
+    constructor () {
+        return this.validate;
     }
 };

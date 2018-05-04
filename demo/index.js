d28c337c9b632f0e35e23bd78a63aa8592119213
Dwayne Ford <dwayne@bl.io>
2018-05-04 05:29:55 -0400

    Test-like demo with v2

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,54 @@
-// const validator = require('../src/validate');
-const validator = require('..');
-const validate = validator.start();
+// const Validator = require('../src/validate');
+const Validator = require('..');
+const rules = Validator.rules;
 
-console.log(validator.rulesets);
+// Validator.rules({
+//     isTwilio: (input) => {
+//         return rules.length(input.To, 12) &&
+//             rules.length(input.From, 12) &&
+//             rules.startsWith(input.To, '+1') &&
+//             rules.startsWith(input.From, '+1');
+//     }
+// });
 
-console.log(validate('000-123-ABC', ['startsWith 000-', 'endsWith ABC', 'alphanumeric -']));
+const validate = new Validator;
+
+
+const env = {
+    "To": "+15553331234",
+    "From": "+15558880000",
+    "Body": "Hello"
+};
+
+test(true, validate('000-123-ABC', [
+  'startsWith 000-',
+  'endsWith ABC',
+  'alphanumeric -']
+));
+
+test(true, validate(env, [
+    'To.startsWith +',
+    'To.length 12',
+    'From.startsWith +',
+    'Body.startsWith H'
+]));
+
+test(false, validate(env, [
+    'To.startsWith _',
+    'To.length 12',
+    'From.startsWith 0',
+    'Body.startsWith H'
+]));
+
+test(true, validate(env, 'To.startsWith +'));
+test(true, validate(env.To, 'startsWith +'));
+test(false, validate(env.From, 'startsWith _'));
+test(true, validate(env.From, 'startsWith +'));
+
+function test(conditionToTest, expectedResult) {
+	var failMsg = '\033[1;31mFail: \033[0m' + `${expectedResult} expected but got ${conditionToTest}`,
+		  passMsg = '\033[1;32mPass: \033[0m' + `${expectedResult} === ${conditionToTest}`,
+		  conditionPasses = conditionToTest === expectedResult;
+
+	console.log(conditionPasses ? passMsg : failMsg);
+}

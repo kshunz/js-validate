6c9ac26ea7d40e8659e0a16a980c0ed810a2e7cd
Dwayne Ford <dwayne@bl.io>
2018-06-30 13:03:17 -0400

    Test linting

--- tests/rules/alphanumeric_tests.js
+++ tests/rules/alphanumeric_tests.js
@@ -17,19 +17,19 @@ describe('Alphanumeric character rule validator', function() {
     expect(alphanumeric('años')).to.be.false;
     expect(alphanumeric('José')).to.be.false;
   });
-  it('should accept numbers only', function () {
+  it('should accept numbers only', function() {
     /*jshint -W046 */
     expect(alphanumeric(1232)).to.be.true;
     expect(alphanumeric(000222)).to.be.true;
     expect(alphanumeric('00324')).to.be.true;
     expect(alphanumeric('123456')).to.be.true;
   });
-  it('should reject multi-word phrases and phrases with non-alphanumeric characters', function () {
+  it('should reject multi-word phrases and phrases with non-alphanumeric characters', function() {
     expect(alphanumeric('Hello World')).to.be.false;
     expect(alphanumeric('I am #1')).to.be.false;
     expect(alphanumeric('ABC 123')).to.be.false;
   });
-  it('should accept multi-word phrases and non-alphanumeric characters if the chars are specified in the rule', function () {
+  it('should accept multi-word phrases and non-alphanumeric characters if the chars are specified in the rule', function() {
     expect(alphanumeric('Hello World', '_space_')).to.be.true;
     expect(alphanumeric('I am #1', '_space_', '#')).to.be.true;
     expect(alphanumeric('ABC 123', '_space_')).to.be.true;
